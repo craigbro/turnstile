8e44072e509ff85096d5eeef8ddf5ff8c3e94ae0
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2018-09-10 13:39:34 +0200

    Update with the latest version of carmine

--- src/turnstile/core.clj
+++ src/turnstile/core.clj
@@ -1,7 +1,6 @@
 (ns turnstile.core
   (:require [taoensso.carmine :as car]))
 
-
 (defprotocol Turnstile
   (expire-entries [this now-ms])
   (space [this limit])
@@ -14,32 +13,32 @@
 (defrecord RedisTurnstile [conn-spec pool name expiration-ms]
   Turnstile
   (expire-entries [this now-ms]
-    (car/with-conn pool conn-spec
+    (car/wcar pool conn-spec
       ;; remove all entries whose score is more them expiration-ms + 1 old (relative to now-ms)
       (car/zremrangebyscore name 0 (- now-ms expiration-ms 1)))
     this)
   (space [this limit]
-    (- limit (car/with-conn pool conn-spec (car/zcard name))))
+    (- limit (car/wcar pool conn-spec (car/zcard name))))
   (has-space? [this limit]
-    (<  (car/with-conn pool conn-spec
+    (<  (car/wcar pool conn-spec
           (car/zcard name)) limit))
   (next-slot [this limit now-ms]
-    (if-let [earliest (first (car/with-conn pool conn-spec (car/zrange name 0 1)))]
-      (let [request-time (or (read-string (car/with-conn pool conn-spec (car/zscore name earliest)))
+    (if-let [earliest (first (car/wcar pool conn-spec (car/zrange name 0 1)))]
+      (let [request-time (or (read-string (car/wcar conn-spec (car/zscore name earliest)))
                              0)]
         (max 0 (- (+ expiration-ms request-time)
                   now-ms)))
       0))
   (add-timed-item [this item time-ms]
-    (car/with-conn pool conn-spec
+    (car/wcar pool conn-spec
       (car/zadd name time-ms item)
       (car/pexpire name (+ expiration-ms 1000))
       this))
   (delete-item [this item]
-    (car/with-conn pool conn-spec
+    (car/wcar pool conn-spec
       (car/zrem name item))
     this)
   (reset [this]
-    (car/with-conn pool conn-spec
+    (car/wcar pool conn-spec
       (car/del name))
     this))
